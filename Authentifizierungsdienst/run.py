e6b37b3c1eb282b111901e07bf7c43ff172b928f
Vincent Hundeloh <s200355@student.dhbw-mannheim.de>
2022-11-26 16:21:44 +0100

    Fixed accidential closing of processes
    ~ Changed run scripts not to close processes immediately

--- Authentifizierungsdienst/run.py
+++ Authentifizierungsdienst/run.py
@@ -5,6 +5,13 @@ import time
 
 APP_NAME = "authentication"
 
+def _read_all(process):
+    while True:
+        line = process.stdout.readline()
+        if line is None:
+            break
+        print(line)
+
 if __name__ == "__main__":
     failures = 0
     while True:
@@ -19,7 +26,7 @@ if __name__ == "__main__":
             time.sleep(1)
     print("Database is up")
 
-    subprocess.Popen(["python", "manage.py", "makemigrations", APP_NAME], shell=True, cwd=".", env=None)
-    subprocess.Popen(["python", "manage.py", "migrate"], shell=True, cwd=".", env=None)
-    subprocess.Popen(["python", "manage.py", "migrate", APP_NAME], shell=True, cwd=".", env=None)
-    subprocess.Popen(["python", "manage.py", "runserver", "0.0.0.0:8000"], shell=True, cwd=".", env=None)
+    _read_all(subprocess.Popen(["python", "manage.py", "makemigrations", APP_NAME], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
+    _read_all(subprocess.Popen(["python", "manage.py", "migrate"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
+    _read_all(subprocess.Popen(["python", "manage.py", "migrate", APP_NAME], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
+    _read_all(subprocess.Popen(["python", "manage.py", "runserver", "0.0.0.0:8000"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))

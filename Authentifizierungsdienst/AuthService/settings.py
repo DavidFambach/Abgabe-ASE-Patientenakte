0018bf86e4c511331bbdc8864ecc800809cdb808
DavidFambach <david.fambahc@gmx.de>
2022-11-27 13:51:19 +0100

    Added credentials for the email host to the environment variables

--- Authentifizierungsdienst/AuthService/settings.py
+++ Authentifizierungsdienst/AuthService/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "djangopatientenakte@gmail.com"
-EMAIL_HOST_PASSWORD = "jemfompwbdjkvlui"
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]

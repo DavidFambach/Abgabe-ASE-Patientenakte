530872ece95abd231432f4ce2955e72ebe28b0c8
Vincent Hundeloh <s200355@student.dhbw-mannheim.de>
2022-11-30 09:22:21 +0100

    Added nginx to servce Django website
    + Added nginx to authentication service
    + Added nginx confguration to authentication service
    + Added nginx to file service
    + Added nginx confguration to file service
    ~ fixed encoding of requirements.txt in authentication service

--- Dateiverwaltungsdienst/run.py
+++ Dateiverwaltungsdienst/run.py
@@ -3,7 +3,16 @@ import psycopg2
 import subprocess
 import time
 
+from threading import Thread
+
 APP_NAME = "app_file"
+
+class RunnableThread(Thread):
+    def __init__(self, r):
+        super().__init__()
+        self.r = r
+    def run(self) -> None:
+        self.r()
 
 def _read_all(process):
     status = process.poll()
@@ -13,6 +22,9 @@ def _read_all(process):
             break
         print(line.decode("utf-8"))
         status = process.poll()
+
+def _run_nginx():
+    _read_all(subprocess.Popen(["nginx", "-g", "daemon off;"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
 
 if __name__ == "__main__":
     failures = 0
@@ -28,7 +40,11 @@ if __name__ == "__main__":
             time.sleep(1)
     print("Database is up")
 
+    nginx_thread = RunnableThread(_run_nginx)
+    nginx_thread.daemon = True
+    nginx_thread.start()
+
     _read_all(subprocess.Popen(["python", "manage.py", "makemigrations", APP_NAME], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
     _read_all(subprocess.Popen(["python", "manage.py", "migrate"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
     _read_all(subprocess.Popen(["python", "manage.py", "migrate", APP_NAME], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
-    _read_all(subprocess.Popen(["python", "manage.py", "runserver", "0.0.0.0:8000"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))
+    _read_all(subprocess.Popen(["python", "-m", "waitress", "--listen=*:8000", "fileservice.wsgi:application"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT))

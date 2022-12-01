6e6a7cc5b7faeba4242663f9b88d0bd2528724a7
DavidFambach <david.fambahc@gmx.de>
2022-12-01 01:29:52 +0100

    close issue #27

--- Authentifizierungsdienst/social_auth/serializers.py
+++ Authentifizierungsdienst/social_auth/serializers.py
@@ -19,7 +19,7 @@ class GoogleSocialAuthSerializer(serializers.Serializer):
 
         if user_data['aud'] != os.environ.get('GOOGLE_CLIENT_ID'):
 
-            raise AuthenticationFailed('oops, who are you?')
+            raise AuthenticationFailed('GOOGLE_CLIENT_ID is invalid')
 
         user_id = user_data['sub']
         email = user_data['email']

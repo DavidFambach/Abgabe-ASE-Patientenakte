649d38113be462ca3e85fcc774457f114fb16f5d
Vincent Hundeloh <s200355@student.dhbw-mannheim.de>
2022-12-05 15:24:12 +0100

    Added dynamic data loading to frontend project
    + Added endpoint configuration file to frontend project
    + Added request to file service to frontend project
    ~ Fixed incorrect key name for userinfo response in file service

---
+++ Webserver/frontend/src/config.js
@@ -0,0 +1,6 @@
+export default Object.freeze({
+    ENDPOINTS: {
+        AUTHENTICATION: "/auth/",
+        FILE: "/file/"
+    }
+})
---
+++ Webserver/frontend/src/fileservice.js
@@ -0,0 +1,35 @@
+import config from "./config"
+import { requestAsync } from "./util";
+
+let USER_ID = null;
+let TOKEN = null;
+
+function getDefaultHeaders() {
+    return {
+        "Authorization": "Bearer " + TOKEN
+    };
+}
+
+export function setOwnUserID(userID) {
+    USER_ID = userID;
+}
+
+export function setOwnToken(token) {
+    TOKEN = token;
+}
+
+export async function getUserInfo() {
+    return await requestAsync(
+        "GET",
+        config.ENDPOINTS.FILE + "userinfo/" + USER_ID,
+        getDefaultHeaders()
+    )
+}
+
+export async function getDirectory(directoryID) {
+    return await requestAsync(
+        "GET",
+        config.ENDPOINTS.FILE + "dir/" + directoryID + "?user=" + USER_ID,
+        getDefaultHeaders()
+    )
+}

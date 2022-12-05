ac69df0cb02095ad9b72287a1ab8e864a72a2d0b
Vincent Hundeloh <s200355@student.dhbw-mannheim.de>
2022-12-05 08:04:45 +0100

    Changed file service to use the configured django secret
    ~ Changed file service to use the django secret from the environment

--- Dateiverwaltungsdienst/fileservice/settings.py
+++ Dateiverwaltungsdienst/fileservice/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#&-^fbspvu6lxm-4m2t=n5l1s-om!-@e#6ft5igpz4y(6$#zhc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = "DEBUG" in os.environ and str(os.environ["DEBUG"]).lower() == "true"
 

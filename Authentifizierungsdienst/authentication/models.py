eafd671f24df1950c769d379fffc7a2ce37da0e8
DavidFambach <david.fambahc@gmx.de>
2022-12-08 19:59:24 +0100

    The user ID is now collision-resistant in the range up to 5 bytes

--- Authentifizierungsdienst/authentication/models.py
+++ Authentifizierungsdienst/authentication/models.py
@@ -40,7 +40,7 @@ AUTH_PROVIDERS = {'google': 'google', 'email': 'email'}
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.BigAutoField(primary_key=True, unique=True, default=random.randint(10000, 99999), editable=False)
+    id = models.BigAutoField(primary_key=True, unique=True, blank=True, editable=False)
     username = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True, db_index=True)
     is_verified = models.BooleanField(default=False)
@@ -60,6 +60,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+    def save(self, *args, **kwargs):
+        while not self.id:
+            newid = random.randbytes(5)
+            if not self.objects.filter(pk=newid).exists():
+                self.id = int.from_bytes(newid, "big")
+        super.save(*args, **kwargs)
     def tokens(self):
         extra_data = {
             "user_name": self.username,

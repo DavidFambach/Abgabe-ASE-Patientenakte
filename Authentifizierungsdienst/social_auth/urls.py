e94f5871fbc0edd71ff868d42e3b07754b9b4b65
DavidFambach <david.fambahc@gmx.de>
2022-12-11 18:21:29 +0100

    add client-id API

--- Authentifizierungsdienst/social_auth/urls.py
+++ Authentifizierungsdienst/social_auth/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import GoogleSocialAuthView
+from .views import GoogleSocialAuthView, GoogleClientIDView
 
 urlpatterns = [
     path('google/', GoogleSocialAuthView.as_view()),
+    path('google/client-id', GoogleClientIDView.as_view()),
 ]
--- Authentifizierungsdienst/social_auth/views.py
+++ Authentifizierungsdienst/social_auth/views.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
@@ -21,3 +23,8 @@ class GoogleSocialAuthView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         data = ((serializer.validated_data)['auth_token'])
         return Response(data, status=status.HTTP_200_OK)
+
+class GoogleClientIDView(GenericAPIView):
+    def get(self, request):
+        client_id = os.getenv("GOOGLE_CLIENT_ID")
+        return Response(client_id, status=status.HTTP_200_OK)

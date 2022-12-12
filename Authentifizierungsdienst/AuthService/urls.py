982adbb0912ae0f24014887badfe952be8b89a52
DavidFambach <david.fambahc@gmx.de>
2022-12-12 14:20:07 +0100

    disable unused API endpoint for admin site

--- Authentifizierungsdienst/AuthService/urls.py
+++ Authentifizierungsdienst/AuthService/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
     path('', include(('social_auth.urls', 'social_auth'),
                                  namespace="social_auth"))

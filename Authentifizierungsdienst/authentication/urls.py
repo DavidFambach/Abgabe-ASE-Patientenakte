e44347ad34b741d02a6914d0753bc6b9d0f8169e
DavidFambach <david.fambahc@gmx.de>
2022-12-12 13:57:18 +0100

    disable unused API endpoints for password reset

--- Authentifizierungsdienst/authentication/urls.py
+++ Authentifizierungsdienst/authentication/urls.py
@@ -10,12 +10,6 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view(), name="logout"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
-         name="request-reset-email"),
-    path('password-reset/<uidb64>/<token>/',
-         PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
-    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
-         name='password-reset-complete'),
     path('password-change/',
          ChangePasswordAPIView.as_view(), name='password-reset-complete'),
     path('delete/', DeleteAccount.as_view(),  name='delete-user-account')
